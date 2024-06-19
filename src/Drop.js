b9bf9f35e2d3c8e0205789a78d01991d75642369
Harshav111 <sj.harsha2382003@gmail.com>
2024-06-19 15:06:02 +0530

    Fourth stage

--- src/Drop.js
+++ src/Drop.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Select from 'react-select';
+import {
+    Box
+} from '@chakra-ui/react';
 
-// Define the static data
+
 const staticData = [
-  { "userId": 1, "id": 1, "title": "delectus aut autem", "completed": false },
-  { "userId": 1, "id": 2, "title": "quis ut nam facilis et officia qui", "completed": false },
-  { "userId": 1, "id": 3, "title": "fugiat veniam minus", "completed": false },
-  { "userId": 1, "id": 4, "title": "et porro tempora", "completed": true },
-  { "userId": 1, "id": 5, "title": "laboriosam mollitia et enim quasi adipisci quia provident illum", "completed": false },
-  { "userId": 1, "id": 6, "title": "qui ullam ratione quibusdam voluptatem quia omnis", "completed": false },
-  { "userId": 1, "id": 7, "title": "illo expedita consequatur quia in", "completed": false },
-  { "userId": 1, "id": 8, "title": "quo adipisci enim quam ut ab", "completed": true },
-  { "userId": 1, "id": 9, "title": "molestiae perspiciatis ipsa", "completed": false },
-  { "userId": 1, "id": 10, "title": "illo est ratione doloremque quia maiores aut", "completed": true }
+  { "userId": 1, "id": 1, "title": "The production activity File", "completed": false },
+  { "userId": 1, "id": 2, "title": "Previous record File", "completed": false },
+  { "userId": 1, "id": 3, "title": "Temporary activities file", "completed": false },
+  { "userId": 1, "id": 4, "title": "Data Recovery File", "completed": true },
+  { "userId": 1, "id": 5, "title": "Data breach File", "completed": false },
+  { "userId": 1, "id": 6, "title": "Total porduction commits", "completed": false },
+  { "userId": 1, "id": 7, "title": "Document  test", "completed": false },
+  { "userId": 1, "id": 8, "title": "Document Update ", "completed": true },
+  { "userId": 1, "id": 9, "title": "Deployed version lists", "completed": false },
+  { "userId": 1, "id": 10, "title": "Rates of the dealed products", "completed": true }
 ];
 
 
@@ -23,7 +26,9 @@ const options = staticData.map(item => ({
 
 const MyComponent = () => {
   return (
+    <Box p={3} bg="teal.50" borderRadius="md" textAlign="center">
     <Select options={options} />
+    </Box>
   );
 };
 
