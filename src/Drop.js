2c6aacbd7325287f764f9b8ca4fada40554d52d0
Harshav111 <sj.harsha2382003@gmail.com>
2024-06-19 16:13:01 +0530

    fifth commit

--- src/Drop.js
+++ src/Drop.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Select from 'react-select';
 import {
-    Box
+    Box,
+    FormControl,
+    FormLabel,
+    FormHelperText
 } from '@chakra-ui/react';
 
 
 const staticData = [
-  { "userId": 1, "id": 1, "title": "The production activity File", "completed": false },
-  { "userId": 1, "id": 2, "title": "Previous record File", "completed": false },
-  { "userId": 1, "id": 3, "title": "Temporary activities file", "completed": false },
-  { "userId": 1, "id": 4, "title": "Data Recovery File", "completed": true },
-  { "userId": 1, "id": 5, "title": "Data breach File", "completed": false },
-  { "userId": 1, "id": 6, "title": "Total porduction commits", "completed": false },
-  { "userId": 1, "id": 7, "title": "Document  test", "completed": false },
-  { "userId": 1, "id": 8, "title": "Document Update ", "completed": true },
-  { "userId": 1, "id": 9, "title": "Deployed version lists", "completed": false },
-  { "userId": 1, "id": 10, "title": "Rates of the dealed products", "completed": true }
+  { "userId": 1, "id": 1, "title": " The production activity File", "completed": false },
+  { "userId": 1, "id": 2, "title": " Previous record File", "completed": false },
+  { "userId": 1, "id": 3, "title": " Temporary activities file", "completed": false },
+  { "userId": 1, "id": 4, "title": " Data Recovery File", "completed": true },
+  { "userId": 1, "id": 5, "title": " Data breach File", "completed": false },
+  { "userId": 1, "id": 6, "title": " Total porduction commits", "completed": false },
+  { "userId": 1, "id": 7, "title": " Document  test", "completed": false },
+  { "userId": 1, "id": 8, "title": " Document Update ", "completed": true },
+  { "userId": 1, "id": 9, "title": " Deployed version lists", "completed": false },
+  { "userId": 1, "id": 10, "title":" Rates of the dealed products", "completed": true }
 ];
 
 
@@ -26,9 +29,13 @@ const options = staticData.map(item => ({
 
 const MyComponent = () => {
   return (
-    <Box p={3} bg="teal.50" borderRadius="md" textAlign="center">
+    <FormControl>
+        <FormLabel color="teal.600" textAlign="center"><b>Enter the File name or id</b></FormLabel>
+    <Box p={5} bg="teal.50" borderRadius="md" textAlign="center">
     <Select options={options} />
     </Box>
+    <FormHelperText textAlign="center" textColor="Black">id:1-10</FormHelperText>
+    </FormControl>
   );
 };
 
